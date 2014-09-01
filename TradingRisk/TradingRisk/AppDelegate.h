650d4164f28bbcc6e6ec7e431f0a86a1bb92842a
Hector Alvarado <halvarad@fiec.espol.edu.ec>
2014-09-01 10:25:36 -0500

    commit inicial

---
+++ TradingRisk/TradingRisk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TradingRisk
+//
+//  Created by Hector on 9/1/14.
+//  Copyright (c) 2014 Hector. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TradingRisk/TradingRisk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TradingRisk
+//
+//  Created by Hector on 9/1/14.
+//  Copyright (c) 2014 Hector. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
